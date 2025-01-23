e53a085a431bfac9227a299b1c6da269870a5369
DouglaSantos777 <fdsantosdec@aluno.uespi.br>
2025-01-23 15:45:10 -0300

    feat: add directory dto and event request dto

---
+++ ms-event/src/main/java/com/desafio03/ms_event/dto/EventRequest.java
@@ -0,0 +1,18 @@
+package com.desafio03.ms_event.dto;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.*;
+import java.time.LocalDateTime;
+
+public record EventRequest(
+        @NotNull(message = "Event Name is required")
+        String eventName,
+        @NotNull(message = "Date Time is required")
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+        LocalDateTime dateTime,
+        @NotNull(message = "cep is required")
+        @Pattern(regexp = "\\d{5}-\\d{3}", message = "Invalid CEP format")
+        String cep
+) {
+}
