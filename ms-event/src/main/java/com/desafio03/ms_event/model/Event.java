c2f4001b15d68168fc732f444984df7fd6bfaf9d
DouglaSantos777 <fdsantosdec@aluno.uespi.br>
2025-01-23 15:35:32 -0300

    chore: updating version of spring boot in maven dependencies

---
+++ ms-event/src/main/java/com/desafio03/ms_event/model/Event.java
@@ -0,0 +1,25 @@
+package com.desafio03.ms_event.entities;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+
+@Document(collection = "tb_events")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class EventEntity {
+    @Id
+    private String id;
+    private String eventName;
+    private LocalDateTime dateTime;
+    private String cep;
+    private String publicPlace;
+    private String district;
+    private String city;
+    private String uf;
+
+}
