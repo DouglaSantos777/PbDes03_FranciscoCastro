d7c49f81893150771cb757fd11b4e28787c1c2c9
DouglaSantos777 <fdsantosdec@aluno.uespi.br>
2025-01-24 10:26:04 -0300

    refactor: updating post, get and getAll methods in EventService with use of EventMapper

--- ms-event/src/main/java/com/desafio03/ms_event/service/EventService.java
+++ ms-event/src/main/java/com/desafio03/ms_event/service/EventService.java
@@ -2,6 +2,7 @@ package com.desafio03.ms_event.service;
 
 import com.desafio03.ms_event.dto.EventRequestDto;
 import com.desafio03.ms_event.dto.EventResponseDto;
+import com.desafio03.ms_event.dto.mapper.EventMapper;
 import com.desafio03.ms_event.model.Event;
 import com.desafio03.ms_event.repositories.EventRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,26 +19,23 @@ public class EventService {
     private final EventRepository eventRepository;
 
     public EventResponseDto createEvent(EventRequestDto eventRequestDto) {
-        Event event = Event.builder()
-                .eventName(eventRequestDto.eventName())
-                .dateTime(eventRequestDto.dateTime())
-                .cep(eventRequestDto.cep())
-                .build();
+        Event event = EventMapper.toEvent(eventRequestDto);
         eventRepository.save(event);
         log.info("Event created successfully");
-        return new EventResponseDto(event.getId(), event.getEventName(), event.getDateTime(), event.getCep(), event.getPublicPlace(), event.getDistrict(), event.getCity(), event.getUf());
+        return EventMapper.toResponseDto(event);
     }
 
     public List<EventResponseDto> getAllEvents(){
         return eventRepository.findAll()
                 .stream()
-                .map(event -> new EventResponseDto(event.getId(), event.getEventName(), event.getDateTime(), event.getCep(), event.getPublicPlace(), event.getDistrict(), event.getCity(), event.getUf()))
+                .map(EventMapper::toResponseDto)
                 .toList();
     }
 
     public EventResponseDto getEvent(String id){
-       Optional<Event> event = eventRepository.findById(id);
-       return new EventResponseDto(event.get().getId(), event.get().getEventName(), event.get().getDateTime(), event.get().getCep(), event.get().getPublicPlace(), event.get().getDistrict(), event.get().getCity(), event.get().getUf());
-    }
+       return eventRepository.findById(id)
+               .map(EventMapper::toResponseDto)
+               .orElseThrow(() -> new RuntimeException("Event not found with ID: " + id));
 
+    }
 }
