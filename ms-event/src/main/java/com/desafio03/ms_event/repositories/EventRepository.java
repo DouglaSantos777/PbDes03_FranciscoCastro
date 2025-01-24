9f17d7d7e8b15ed28d7ff66b1629fea530aeae98
DouglaSantos777 <fdsantosdec@aluno.uespi.br>
2025-01-24 06:33:09 -0300

    feat: add Event repository

---
+++ ms-event/src/main/java/com/desafio03/ms_event/repositories/EventRepository.java
@@ -0,0 +1,7 @@
+package com.desafio03.ms_event.repositories;
+
+import com.desafio03.ms_event.model.Event;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface EventRepository extends MongoRepository<Event, String> {
+}
