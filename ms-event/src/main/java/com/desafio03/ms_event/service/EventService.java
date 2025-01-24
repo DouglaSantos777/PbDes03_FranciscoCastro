d271353e4659fe0f84b41a90b0e7309d9a1c7ea0
DouglaSantos777 <fdsantosdec@aluno.uespi.br>
2025-01-24 06:42:58 -0300

    feat: add directory service and EventService with Create Event method

--- ms-event/src/main/java/com/desafio03/ms_event/service/EventService.java
+++ ms-event/src/main/java/com/desafio03/ms_event/service/EventService.java
@@ -1,4 +1,26 @@
 package com.desafio03.ms_event.service;
 
+import com.desafio03.ms_event.dto.EventRequestDto;
+import com.desafio03.ms_event.model.Event;
+import com.desafio03.ms_event.repositories.EventRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class EventService {
+    private final EventRepository eventRepository;
+
+    public void createProduct(EventRequestDto eventRequestDto) {
+        Event event = Event.builder()
+                .eventName(eventRequestDto.eventName())
+                .dateTime(eventRequestDto.dateTime())
+                .cep(eventRequestDto.cep())
+                .build();
+        eventRepository.save(event);
+        log.info("Event created successfully");
+    }
+
 }
