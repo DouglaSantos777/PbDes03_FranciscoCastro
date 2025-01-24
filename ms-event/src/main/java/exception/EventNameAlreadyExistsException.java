2b9af27a58ab303f51925602c212a181712813b2
DouglaSantos777 <fdsantosdec@aluno.uespi.br>
2025-01-24 13:21:45 -0300

    feat: add Exceptions for Events with name that already exists

---
+++ ms-event/src/main/java/exception/EventNameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class EventNameAlreadyExistsException extends RuntimeException{
+    public EventNameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
