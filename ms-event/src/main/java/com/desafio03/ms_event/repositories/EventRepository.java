db1f6f75490e8d0f655ad7f4fabe60ce9b8b8fe5
DouglaSantos777 <fdsantosdec@aluno.uespi.br>
2025-01-24 13:22:24 -0300

    feat: add method in Event Repository for Event Names that already exists

--- ms-event/src/main/java/com/desafio03/ms_event/repositories/EventRepository.java
+++ ms-event/src/main/java/com/desafio03/ms_event/repositories/EventRepository.java
@@ -1,7 +1,9 @@
 package com.desafio03.ms_event.repositories;
 
+import com.desafio03.ms_event.dto.EventRequestDto;
 import com.desafio03.ms_event.model.Event;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface EventRepository extends MongoRepository<Event, String> {
+    boolean existsByEventName(Event event);
 }
