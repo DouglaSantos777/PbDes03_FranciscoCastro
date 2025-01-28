28e3fe0653f961de1149126a5db216af0e7d0feb
DouglaSantos777 <fdsantosdec@aluno.uespi.br>
2025-01-28 08:19:56 -0300

    feat: add request and response DTO for Ticket

---
+++ ms-ticket/src/main/java/com/desafio03/ms_ticket/model/dto/TicketRequestDto.java
@@ -0,0 +1,15 @@
+package com.desafio03.ms_ticket.model.dto;
+
+import com.desafio03.ms_ticket.clientevents.EventResponseDto;
+
+public record TicketRequestDto(
+    String ticketId,
+    String customerName,
+    String cpf,
+    String customerMail,
+    String eventId,
+    String BRLtotalAmount,
+    String USDtotalAmount
+){
+}
+
---
+++ ms-ticket/src/main/java/com/desafio03/ms_ticket/model/dto/TicketResponseDto.java
@@ -0,0 +1,4 @@
+package com.desafio03.ms_ticket.model.dto;
+
+public class TicketResponseDto {
+}
