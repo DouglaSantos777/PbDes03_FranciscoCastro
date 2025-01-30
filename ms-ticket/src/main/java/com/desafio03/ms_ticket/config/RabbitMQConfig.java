ec150e5dfda2d822ead42c60d119e89fc3d86349
DouglaSantos777 <fdsantosdec@aluno.uespi.br>
2025-01-30 00:53:48 -0300

    feat, chore: add initial rabbitmq setup

---
+++ ms-ticket/src/main/java/com/desafio03/ms_ticket/config/RabbitMQConfig.java
@@ -0,0 +1,20 @@
+/*
+package com.desafio03.ms_ticket.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.template.default-receive-queue}")
+    private String queue;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queue, true);
+    }
+}
+*/
